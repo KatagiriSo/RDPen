0af7338c803784240c8bf64b860f90021d63baf7
katagiri <So.Katagiri@gmail.com>
2014-07-16 13:12:28 +0900

    Initial Commit

---
+++ RDPen/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RDPen
+//
+//  Created by 片桐奏羽 on 2014/07/16.
+//  Copyright (c) 2014年 Rodhos Soft. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
